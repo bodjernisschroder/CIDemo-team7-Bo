848f8e9ec0aa120085d3de2159c2bddd9c2739e3
bodjernisschroder <bdsc63266@edu.ucl.dk>
2025-04-10 17:06:45 +0200

    asdasd

--- CalculatorApp/Calculator.cs
+++ CalculatorApp/Calculator.cs
@@ -4,7 +4,7 @@
     {
         public int Add(int a, int b) => a + b;
 
-        public int Subtract(int a, int b) => a + b;
+        public int Subtract(int a, int b) => a - b;
 
         public int Multiply(int a, int b) => a * b;
 
